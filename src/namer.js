7d05b4b8e420631d6cb8b6a29fe9f5534534f657
Andreas Weber <andreas@chromawoods.com>
2016-08-02 16:47:40 +0200

    Don't accept duplicate fragments by default
    acceptDuplicateFrags has been added to prevent duplicate name-fragments
    in the same name, for example "Batman Batman".

--- src/namer.js
+++ src/namer.js
@@ -6,12 +6,14 @@
 
     // Defaults
     this.defaults = {
-      numPos: 2
+      numPos: 2,
+      acceptDuplicateFrags: false
     };
 
     // Attributes
     this.fragments = _compileFragments(fragments);
     this.numPos = numPos || this.defaults.numPos;
+    this.acceptDuplicateFrags = this.defaults.acceptDuplicateFrags;
     this.history = [];
     this.currentName = { fragments: null, fullName: null };
 
@@ -85,16 +87,35 @@
     the name as a fragment array, and a fullName as human readable text. */
   var _getName = function(numPos) {
 
-    var frags = [], tmpFrag, tmpFragIndex;
+    var frags = [], self = this;
+
+    // Get a single name-fragment from an array of fragments
+    function getFragment(tmpFrags, iterations) {
+
+      var tmpFragIndex = Math.floor(Math.random() * (tmpFrags.length)),
+        tmpFrag = tmpFrags[tmpFragIndex];
+
+      // Function calls itself if unwanted duplicate is detected
+      if (self.acceptDuplicateFrags === false && frags.join().indexOf(tmpFrag) >= 0) {
+
+        // "iterations" prevents enless recursion (even though it should never happen)
+        iterations = iterations || 0;
+
+        return (iterations > 99) ? '' : getFragment(tmpFrags, iterations + 1);
+      }
+
+      else {
+        return tmpFrag;
+      }
+
+    }
 
     numPos = numPos || this.numPos;
 
     for (var i = 1; i < numPos + 1; i++) {
 
       if (this.fragments[i] && this.fragments[i].length) {
-        tmpFrag = this.fragments[i];
-        tmpFragIndex = Math.floor(Math.random() * (tmpFrag.length));
-        frags.push(tmpFrag[tmpFragIndex]);
+        frags.push(getFragment(this.fragments[i]));
       }
 
     }
